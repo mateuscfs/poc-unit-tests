3501ec39aded6a9646fc6fabff108d4894e0dde6
Mateus Coelho <mateuscfs@gmail.com>
2023-08-01 15:49:27 -0300

    feat: add error validation

---
+++ src/utils/validation/validationError.ts
@@ -0,0 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ValidationError } from 'express-validator';
+import { HttpError } from '../errors';
+
+const extractErrors = (validationErrors: ValidationError[]) => {
+    let paramsErrors = validationErrors.map(error => error.param);
+    validationErrors.forEach((error: any) => {
+        if (error.nestedErrors) {
+            const nestedErrorsParams = error.nestedErrors.map(
+                (nestedError: { param: any }) => nestedError.param,
+            );
+            paramsErrors = paramsErrors.concat(nestedErrorsParams);
+        }
+    });
+    return paramsErrors.filter(error => error !== '_error');
+};
+
+export const isParamsInValidationErrors = (
+    params: string[],
+    validationErrors: ValidationError[],
+): boolean => {
+    const validErrors = extractErrors(validationErrors);
+    return params.every(error => validErrors.includes(error));
+};
+
+export class ValidatorError extends HttpError {
+    errors: ValidationError[];
+
+    constructor(errors: ValidationError[]) {
+        super(400, 'Bad Request');
+
+        this.errors = errors;
+    }
+}
