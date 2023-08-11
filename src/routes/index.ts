b5b003834347a917c286f87d08f876d3245d79f4
Matheus Lemberg <matheus.lemberg@hotmail.com>
2023-08-11 11:29:32 -0300

    feat: 'routes configs'

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,24 +1,35 @@
 import { Request, Response, Router } from 'express';
-import { matchedData } from 'express-validator';
 import * as controllers from '../controllers';
+import * as indexValidators from '../utils/index/validators';
+import { getValidData } from '../utils/validation/validationHandler';
 
 const routes = Router();
 
 routes.get('/', (req: Request, res: Response) => {
     res.send('Test Service 1.0.0');
+    console.log(res.status);
 });
 
-routes.post('/create', async (req: Request, res: Response) => {
-    const data = <any>matchedData(req, { includeOptionals: false });
-    const result = await controllers.create(data)
-    res.send(result);
-});
-
-routes.get('/:id', async (req: Request, res: Response) => {
-    const data = <any>matchedData(req, { includeOptionals: false });
-    const result = await controllers.getById(data.id)
-    res.send(result);
-});
-
+routes.post(
+    '/create',
+    indexValidators.createValidator,
+    async (req: Request, res: Response) => {
+        const { body, headers } = getValidData(req);
+        const result = await controllers.create({
+            ...body,
+            tenantid: headers.tenantid,
+        });
+        res.status(201).send(result);
+    },
+);
+routes.get(
+    '/:id',
+    indexValidators.getProfileValidator,
+    async (req: Request, res: Response) => {
+        const { params } = getValidData(req);
+        const result = await controllers.getById(params.id);
+        res.status(200).send(result);
+    },
+);
 
 export default routes;
