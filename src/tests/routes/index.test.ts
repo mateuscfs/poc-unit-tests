ac8f66d05c955ca6b6210f45426d0f5042aac763
Matheus Lemberg <matheus.lemberg@hotmail.com>
2023-08-11 12:12:41 -0300

    test: routes tests

--- src/tests/routes/index.test.ts
+++ src/tests/routes/index.test.ts
@@ -1,14 +1,18 @@
 import test from 'ava';
 import sinon from 'sinon';
 import request from 'supertest';
-import * as repository from '../../repositories';
 import * as controller from '../../controllers';
 import { IndexBuilder } from '../testBuilders/indexBuilder';
 import { IndexCreateDefinition } from '../../routes/doc/interfaces/index';
 import definitionsDocumentation from '../../routes/doc';
 import app from '../../app';
+import { isParamsInValidationErrors } from '../../utils/validation/validationError';
 
 const { IndexCreate } = definitionsDocumentation as IndexCreateDefinition;
+
+test.serial.afterEach.always(() => {
+    sinon.restore();
+});
 
 test.serial('Test Service ok', async t => {
     const expected = 'Test Service 1.0.0';
@@ -37,12 +41,42 @@ test.serial('Route /create', async t => {
     t.deepEqual(res.body, body);
 });
 
-test.serial('routes get /:id', async () => {
+test.serial('Route /create error', async t => {
+    const body = new IndexBuilder()
+        .withActive(<any>null)
+        .withArray(<any>null)
+        .withName(<any>null)
+        .withNumber(<any>'invalid')
+        .build();
+
+    const createController = sinon.stub(controller, 'create').returns(<any>body);
+    const res = await request(app).post('/create').send(body);
+
+    t.true(createController.notCalled);
+    t.deepEqual(res.status, 400);
+    t.true(
+        isParamsInValidationErrors(
+            ['active', 'array', 'number', 'name', 'tenantid'],
+            res.body.errors,
+        ),
+    );
+});
+
+test.serial('routes get /:id', async t => {
     const id = 'fakeid';
     const getByIdController = sinon.stub(controller, 'getById').returns(<any>id);
     const res = await request(app).get('/:id').send(id).set('tenantid', 'stringg');
 
     t.true(getByIdController.calledOnce);
-    t.deepEqual(res.status, 201);
+    t.deepEqual(res.status, 200);
     t.deepEqual(res.text, 'fakeid');
+});
+
+test.serial('routes get /:id error', async t => {
+    const id = 'fakeid';
+    const getByIdController = sinon.stub(controller, 'getById').returns(<any>id);
+    const res = await request(app).get('/:id').send(id);
+    t.true(getByIdController.notCalled);
+    t.deepEqual(res.status, 400);
+    t.true(isParamsInValidationErrors(['tenantid'], res.body.errors));
 });
